58a6b9eee7a896bf2b9da253dfd68003f25d4a6a
Choi Jae Yeon <wpdus346314@gmail.com>
2026-05-06 04:04:04 +0900

    수정

--- testcli/Source/testcli/testcli.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "testcli.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, testcli, "testcli" );
--- 언리얼 코드 수정사항/MyProject_Start.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MyProject_Start.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyProject_Start, "MyProject_Start" );
